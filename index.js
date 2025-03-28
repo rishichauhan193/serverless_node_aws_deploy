2b14dba1ccde6068e9608e3fe45c72711da14816
Rishi Chauhan <rishichauhan1419@gmail.com>
2025-03-28 15:12:22 +0530

    added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.get("/", (req, res) => {
+app.get("/api/get", (req, res) => {
     res.send("Hello, Serverless EC2!");
 });
 
