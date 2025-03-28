fa8ec4efd35df19c186b55d4306d44bda6c3c66f
Rishi Chauhan <rishichauhan1419@gmail.com>
2025-03-28 15:15:36 +0530

    Changed Code

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
-const PORT = process.env.PORT || 4000;
 
-app.get("/api/get", (req, res) => {
-    res.send("Hello, Serverless EC2!");
-});
+app.get('/api/get', (req,res) => {
+    res.send({message : 'Nodejs AWS Deployment'});
+})
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+app.get('/api/get/user', (req,res) => {
+    res.send({name : 'Rishi', email: 'rbchauhan111@gmail.com', 'role': 'Devops'});
+})
+
+app.listen(7000, (req,res) => {
+    console.log('server running on port 7000')
+})
